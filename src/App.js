5d8c021bc0a89738f4697fa32fdf2191cbc0ee8c
Bruno Dias <dias.h.bruno@gmail.com>
2023-03-02 11:55:36 -0300

    only authenticated users can count.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import { taggedSum } from 'daggy';
 import { useReducer } from 'react';
 
+const User = taggedSum('User', {
+  'Guest': [],
+  'Authd': ['user']
+});
+
 const AppState = taggedSum('AppState', {
-  'A': ['counter']
+  'A': ['user', 'counter']
 });
 
 const AppEvent = taggedSum('AppEvent', {
   'Increment': ['value']
 });
 
+const { Guest, Authd } = User;
 const { A } = AppState;
 const { Increment } = AppEvent;
 
+const INITIAL_USER_STATE = Guest;
+
+function Login() {
+  return (
+    <p>Only logged users can count</p>
+  );
+}
+
+function Counter({ state }) {
+  return (
+    <div onClick={() => state.dispatch(Increment(1))}>
+      {state.counter.toString()}
+    </div>
+  );
+}
+
 function App() {
   const [state, dispatch] = useReducer(
-    (acc, event) => A(acc.counter + event.value),
-    A(1)
+    (acc, event) => A(acc.user, acc.counter + event.value),
+    A(INITIAL_USER_STATE, 1)
   );
 
   return (
-    <div className="App" onClick={() => dispatch(Increment(1))}>
-      {state.counter.toString()}
+    <div className="App">
+      {state.user.cata({
+	Guest: () => <Login state={{ state, dispatch }} />,
+	Authd: () => <Counter state={{ state, dispatch }} />
+      })}
     </div>
   );
 }
