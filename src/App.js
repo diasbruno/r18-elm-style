12055a7e11d3bbfaf569a847d645ddefd8581a60
Bruno Dias <dias.h.bruno@gmail.com>
2023-03-02 11:41:36 -0300

    example of wrapping the counter on a new type using daggy.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import { taggedSum } from 'daggy';
 import { useReducer } from 'react';
 
+const AppState = taggedSum('AppState', {
+  'A': ['counter']
+});
+
+const { A } = AppState;
+
 function App() {
-  const [counter, dispatch] = useReducer(
-    (counter, item) => counter + item,
-    1
+  const [state, dispatch] = useReducer(
+    (acc, item) => A(acc.counter + item),
+    A(1)
   );
 
   return (
     <div className="App" onClick={() => dispatch(1)}>
-      {counter.toString()}
+      {state.counter.toString()}
     </div>
   );
 }
