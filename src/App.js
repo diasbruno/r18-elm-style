6bfd13f36786e0d330118a13bf90bb2bad1c7c19
Bruno Dias <dias.h.bruno@gmail.com>
2023-03-02 13:55:25 -0300

    added authentication.

--- src/App.js
+++ src/App.js
@@ -1,51 +1,146 @@
 import { taggedSum } from 'daggy';
 import { useReducer } from 'react';
 
+// types
+
 const User = taggedSum('User', {
   'Guest': [],
   'Authd': ['user']
 });
 
+const State = taggedSum('State', {
+  'LoginForm': ['username', 'password'],
+  'LoggingIn': [],
+  'Count': ['value'],
+});
+
 const AppState = taggedSum('AppState', {
-  'A': ['user', 'counter']
+  'A': ['user', 'state'],
 });
 
 const AppEvent = taggedSum('AppEvent', {
+  'UpdateLoginForm': ['field', 'value'],
+  'SubmitLogin': ['username', 'password'],
+  'LoggedIn': ['user'],
+  'LoggingOut': [],
   'Increment': ['value']
 });
 
 const { Guest, Authd } = User;
+const { LoginForm, LoggingIn, Count } = State;
 const { A } = AppState;
-const { Increment } = AppEvent;
+const { UpdateLoginForm, SubmitLogin, LoggedIn, LoggingOut, Increment } = AppEvent;
 
-const INITIAL_USER_STATE = Guest;
+const initialAppState = () => A(Guest, LoginForm('', ''));
 
-function Login() {
+// components
+
+function Login(s) {
+  if (!s) return; // this seems to be a react bug...
+
+  const { dispatch, appstate: { state: { username, password } } } = s;
+  const change = (field) => (input) => dispatch(
+    UpdateLoginForm(field, input.target.value)
+  );
+  const submit = (e) => {
+    e.preventDefault();
+    dispatch(SubmitLogin(username, password));
+  };
   return (
-    <p>Only logged users can count</p>
+    <form onSubmit={submit}>
+      <input type="text"
+             placeholder="username"
+             value={username}
+             onChange={change('username')} />
+      <input type="text"
+             placeholder="password"
+             value={password}
+             onChange={change('password')} />
+      <button type="submit">
+        login
+      </button>
+    </form>
   );
 }
 
-function Counter({ state }) {
+function Counter(s) {
+  if (!s) return; // this seems to be a react bug...
+
+  const { dispatch, appstate: { state } } = s;
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(LoggingOut);
+  };
   return (
-    <div onClick={() => state.dispatch(Increment(1))}>
-      {state.counter.toString()}
+    <div>
+      <p>
+        {state.value.toString()}
+        <button style={{ marginLeft: "10px" }}
+                onClick={() => dispatch(Increment(1))}>
+          increment
+        </button>
+      </p>
+      <button onClick={logout}>logout</button>
     </div>
   );
 }
 
 function App() {
+  // state
   const [state, dispatch] = useReducer(
-    (acc, event) => A(acc.user, acc.counter + event.value),
-    A(INITIAL_USER_STATE, 1)
+    (acc, event) => {
+      // nothing to do...doing nothing!
+      if (!event) return acc;
+
+      // we pattern match on the event
+      // and integrate on the current state.
+      return event.cata({
+        'Increment': (value) =>
+          A(acc.user, Count(acc.state.value + value)),
+        'UpdateLoginForm': (field, value) => {
+          (field === 'username') ?
+              (acc.state.username = value) :
+              (acc.state.password = value);
+          return A(acc.user,
+                   LoginForm(acc.state.username,
+                             acc.state.password));
+        },
+        'SubmitLogin': (username, password) => {
+          Promise.resolve(
+            { name: 'dias' }
+          ).then(
+            user => dispatch(LoggedIn(user))
+          );
+          return A(acc.user, LoggingIn);
+        },
+        'LoggedIn': (user) => A(Authd(user), Count(1)),
+        'LoggingOut': () => initialAppState()
+      });
+    },
+    initialAppState()
   );
 
   return (
     <div className="App">
-      {state.user.cata({
-	Guest: () => <Login state={{ state, dispatch }} />,
-	Authd: () => <Counter state={{ state, dispatch }} />
-      })}
+      { /* in a real application, you would combine
+           with the router */
+        state.user.cata({
+          Guest: () => state.state.cata({
+            LoginForm: () => (
+                <Login appstate={state} dispatch={dispatch} />
+            ),
+            LoggingIn: () => (
+                <div>loading</div>
+            ),
+            // this is required for pattern matching
+            // but this case should be impossible
+            // or it's a bug...
+            Count: () => undefined
+          }),
+          Authd: () => <Counter appstate={state} dispatch={dispatch} />
+        })
+      }
     </div>
   );
 }
